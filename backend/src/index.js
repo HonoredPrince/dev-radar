4bddfd59976737764d13a0dc7bbf0fd34bf02a3e
HonoredPrince <matheuspsnbrasil@hotmail.com>
2021-04-28 21:46:47 -0300

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://honoredprince:porogami32@omnistack-cwqte.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+
+//Query Params: request.query(Filtros, ordenação, paginação...) 
+//Route Params: requesy.params(Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
+
+app.listen(8080);
